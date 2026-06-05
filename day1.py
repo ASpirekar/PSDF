fd14814900ee0fe076065297439213561eeb280e
ASpireKar <smarts.write33@gmail.com>
2026-06-05 19:49:53 +0500

    First Python program

---
+++ day1.py
@@ -0,0 +1,10 @@
+#creating first variable of integer type
+num=25
+#creating second variable of float type
+f=2.345
+#creating third variable of string type
+name="Nabeel Hashmi"
+print(num)
+print(f)
+print(name)
+
