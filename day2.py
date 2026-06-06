a92b12855057383e02e6f425fce58c5b5313ef32
ASpireKar <smarts.write33@gmail.com>
2026-06-06 23:23:35 +0500

    File2 day2 practice

---
+++ day2.py
@@ -0,0 +1,31 @@
+#Type checking of variables
+a=50
+b=5.8
+c="coding makes life interesting"
+d=2+9j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+#printing the parts of complex numbers
+print(d.real)
+print(d.imag)
+
+
+#Specifying the Data type of a variable
+#integer
+var1=int(1)
+var2=int(2.6)
+var3=int("5")
+print(var1)
+print(var2)
+print(var3)
+ #float
+f1=float(4.23)
+f2=float(6)
+f3=float("2.8")
+f4=float("7")
+print(f1)
+print(f2)
+print(f3)
+print(f4)
