1e66d11156e635fc8352a3a1dbe63d30132e5d5a
ASpireKar <smarts.write33@gmail.com>
2026-06-07 09:36:42 +0500

    Last Updated

--- day2.py
+++ day2.py
@@ -3,29 +3,51 @@ a=50
 b=5.8
 c="coding makes life interesting"
 d=2+9j
-print(type(a))
+"""print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
 #printing the parts of complex numbers
 print(d.real)
-print(d.imag)
+print(d.imag)"""
 
 
 #Specifying the Data type of a variable
 #integer
+'''
 var1=int(1)
 var2=int(2.6)
 var3=int("5")
 print(var1)
 print(var2)
 print(var3)
+'''
  #float
 f1=float(4.23)
 f2=float(6)
 f3=float("2.8")
 f4=float("7")
+
+"""
 print(f1)
 print(f2)
 print(f3)
 print(f4)
+"""
+#string casting
+s1=str(3)
+s2=str("1.423")
+s3=str("mystr")
+print(type(s1))
+print(type(s2))
+print(type(s3))
+#Code challenge 
+#creating variable x with integer 1 then conver it into float in new
+#variable a
+x=1
+a=float(1)
+#after conversion in float
+print("after conversion in float", a)
+b=str(x)
+#after converison in string
+print("after conversion in string",b)
