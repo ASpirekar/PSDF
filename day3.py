1f33f249bce45703b24db7fbe7c1a378910064cf
ASpireKar <smarts.write33@gmail.com>
2026-06-08 17:46:55 +0500

    Its day3

---
+++ day3.py
@@ -0,0 +1,14 @@
+#Strings and display it on screen
+print("Welome to my 1st-Repository")
+#Assiging a string to a variable through double ""
+str1="I am a string no.1"
+print(str1) 
+#Assigning a string with three double ""
+lngstr= """Lorem ipsum dolor sit amet, consectetur adipiscing elit. " \
+"Mauris sed semper sem, in egestas diam.Sed molestie ex sed magna interdum,
+ sed mollis risus efficitur."""
+print(lngstr)
+
+#Assiging a string with single '''
+sngstr='''This is signle qouets string assiging to a variable'''
+print(sngstr)
