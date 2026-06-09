44952633d2fbb05ead0abdfd42e1aa131a6fae62
ASpireKar <smarts.write33@gmail.com>
2026-06-09 19:07:21 +0500

    Assignment1

---
+++ Day1/day1.py
@@ -0,0 +1,28 @@
+#creating first variable of integer type
+num=25
+#creating second variable of float type
+f=2.345
+#creating third variable of string type
+name="Nabeel Hashmi"
+"""print(num)
+print(f)
+print(name)"""
+#creating compound asignment statement 
+a,b,c=12,2.15,"HELLO"
+print(a) #printing the value of a
+print(b) #printing the value of b
+print(c) #printing the value of
+#creating multiple  variables with same string value
+str1=str2="Welcome to PSDF"
+print(str1) #printing the value of str1
+print(str2) #printing the value of str2
+#literals in python
+#integer literals
+n=50
+print(n)
+#float literals
+f="2.10"
+print(f)
+s="string literal"
+print(s)
+
