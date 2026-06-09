c0df47bbce4c743e9826cb33baebe3dbd8cc0ea8
ASpireKar <smarts.write33@gmail.com>
2026-06-09 19:16:32 +0500

    Initial commit

---
+++ Day4/list.py
@@ -0,0 +1,25 @@
+#1st list creation with string values
+li=["mango","cherry","apple","orange"]
+print(li)
+
+#list with integer values
+integ=[12,11,23,22,20]
+print(integ)
+
+#list with mixed type values
+mixed=["Ahmad",19,2.4,True]
+print(mixed)
+
+
+#Accessing Items from list
+print(mixed[1:4])
+print(mixed[-3:-5])
+
+#Modifying Items in lists 
+#change by index
+animals=[1,"cat","Horse","Tiger","dog","bird"]
+animals[1]=10
+
+animals[4]="sparrow"
+
+print(animals)
