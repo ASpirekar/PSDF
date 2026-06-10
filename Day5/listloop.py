c6c083c46e69059b101cf6389cfb284743882e15
ASpireKar <smarts.write33@gmail.com>
2026-06-10 19:13:18 +0500

    listloop

---
+++ Day5/listloop.py
@@ -0,0 +1,17 @@
+#list using for loop
+var1=["keyboard","Mouse","Printer","Scanner"]
+#for items in var1:
+  
+#for loop with range function
+
+"""print((range(len(var1))))
+   
+for i in range(len(var1)):
+     print(var1[i])"""
+i=2
+while i<len(var1):
+    print(var1[i])
+    i=i+1
+
+    
+
