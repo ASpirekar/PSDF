6cc5d32a4d91e78e29fc1ecdb780c767b1794f4b
ASpireKar <smarts.write33@gmail.com>
2026-06-18 18:07:56 +0500

    week3

---
+++ week3/dictionary.py
@@ -0,0 +1,40 @@
+thisdict={
+    "myname":"Tayyab",
+    "programming":"python",
+    "subjects":["Database","Networking","Algorithm"],
+    "tupitems":("Laptop","mobile"),
+    "myname":"Usama",
+    "subject":{
+        "computer":56,
+        "Math":34,
+        "English":64,
+        "Physics":45
+    }
+
+}
+"""thisdict["myname"]="Hamza"
+thisdict["newkey"]="keypair"
+print(thisdict["tupitems"])
+print(thisdict["programming"])
+print(thisdict["subjects"])
+print(thisdict["myname"])
+print(thisdict["newkey"])
+
+ print(thisdict["subject"]["computer"])"""
+# print(len(thisdict.keys()))
+# print(len(list(thisdict.keys())))
+# print(list(thisdict.values()))
+
+
+# pairs=thisdict.items()
+#print(pairs[0])
+# print(thisdict.items())
+print(thisdict.get("subject1"))
+print(thisdict["programming"])
+thisdict.update({"City":"Faisalabad"})
+print(thisdict)
+newdict={"city1":"Lahore"}
+thisdict.update(newdict)
+print(thisdict)
+mydict=dict(city="Faisalabad", country="Pakistan")
+print(mydict)
---
+++ week3/if_condition.py
@@ -0,0 +1,40 @@
+"""marks=int(input("Enter your Marks"))
+if(marks>=45):
+    print("you have passed")
+else:
+    print("Sorry! You have failed")"""
+
+
+# WAP that input marks from student and print the grade
+"""score=int(input("Enter your marks"))
+if(score>=90):
+    grade="A"
+elif(score>=80):
+    grade="B"
+elif(score>=70):
+    grade="C"
+elif(score>=60):
+    grade="D"
+elif(score>=50):
+    grade="F"
+else:
+    grade="Fail"
+print("Your Grade is",grade)
+"""
+"""weather=input("Enter the today weather")
+if weather=="Sunny":
+    print("Take Umbrella")
+elif(weather=="Cloudy"):
+    print("Enjoyu your Day")
+elif(weather=="Rainy"):
+    print("Enjoy the rain")
+else:
+    print("Wrong Attempt")"""
+
+
+n1=int(input("Enter first number"))
+n2=int(input("Enter Second number"))
+if(n1>n2):
+    print("numer 1 is greater")
+else:
+    print("number 2 is greater")
